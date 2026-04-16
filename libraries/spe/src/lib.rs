c71a0d606f441e556323f2761a19b419a244480b
Niklas Sirch <niklas.sirch@infineon.com>
2026-04-16 10:21:55 +0200

    flash

--- libraries/spe/src/lib.rs
+++ libraries/spe/src/lib.rs
@@ -6,7 +6,12 @@
 #![feature(abi_cmse_nonsecure_call, cmse_nonsecure_entry)]
 
 pub mod attest;
+mod errorcode;
+pub mod hil;
+pub mod internal_trusted_storage;
 pub mod serial;
 mod service;
 pub mod static_init;
 pub mod veneers;
+
+pub use crate::errorcode::ErrorCode;
