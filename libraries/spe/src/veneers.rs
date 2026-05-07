fd734baa9483591cc08e7fedbfedd6e6649bbba2
Niklas Sirch <niklas.sirch@infineon.com>
2026-05-07 10:31:29 +0200

    veneer comment

--- libraries/spe/src/veneers.rs
+++ libraries/spe/src/veneers.rs
@@ -2,6 +2,11 @@
 // These are placeholders for secure function entry points.
 
 // TODO: Do I need reentrance protection like here: secure_fw/partitions/ns_agent_tz/psa_api_veneers_v80m.c ?
+// 2. Why is Reentrancy a Problem?
+// If a Secure function is not designed to be reentrant (meaning it doesn't handle multiple simultaneous instances of itself), a second call could:
+// Corrupt the Secure Stack: Overwrite local variables or return addresses of the first call.
+// Bypass Security Logic: If a function checks a permission at the start and relies on that state, a reentrant call might alter the state while the first instance is still mid-execution.
+// Leak Data: State from the first "half-finished" call might be accessible to the second call.
 
 // unsafe(no_mangle) is required to ensure these functions are linkable from
 // non-secure code. It is unsafe because there could be name collisions.
