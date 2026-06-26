c567f2de32a801b55b599d1875765d205e3ff9ec
Niklas Sirch <niklas.sirch@infineon.com>
2026-06-26 16:28:19 +0200

    swap uart1 and uart0

--- boards/musca_b1/secure/src/spcb.rs
+++ boards/musca_b1/secure/src/spcb.rs
@@ -7,12 +7,14 @@ pub fn enable_idau_nsc_code() {
     }
 }
 
-pub fn enable_uart0_ns() {
+pub fn enable_uart1_ns() {
     unsafe {
-        // APB_PPCEXP2 is at offset 0x78 from SPCB base 0x50080000
-        const APB_PPCEXP2_ADDR: u32 = 0x50080078u32;
-        let ppc_exp2 = APB_PPCEXP2_ADDR as *mut u32;
-        // Bit 0 controls UART0 security (1 = Non-Secure)
-        *ppc_exp2 |= 1;
+        // APBNSPPCEXP1 is at offset 0x84 from SPCB base 0x50080000
+        const APBNSPPCEXP1_ADDR: u32 = 0x50080084u32;
+        let nsp_ppc_exp1 = APBNSPPCEXP1_ADDR as *mut u32;
+        // WORKAROUND:
+        // In datasheet Bit 5 controls UART0 security (1 = Non-Secure)
+        // In reality it control UART1 somehow or just plain doesn't work idk.
+        *nsp_ppc_exp1 |= 1 << 5;
     }
 }
